704545307715e5469db0a0d186a5903c8ac530e4
RoAd-ZU <ZUZERN.AD@gmail.com>
2023-10-12 10:36:18 +0300

    README.md - ready main.py - added try and except
    DBmanager - ready
    Create DB - ready
    Create table - ready
    Add data - ready

--- main.py
+++ main.py
@@ -5,42 +5,45 @@ from sql import Create_DB, Adding_data, DBManager
 def main():
     password = input("Введите свой пароль postgresql\n")
     dbm = DBManager(password)
-    db = Create_DB(password)
-    db.create_db()
-    tbl = Adding_data(password)
-    tbl.create_table()
+    try:
+        db = Create_DB(password)
+        db.create_db()
+        tbl = Adding_data(password)
+        tbl.create_table()
 
-    company = {'ЭкоПоинт': '5864001',
-               'ООО АСД Технолоджиз': '5088268',
-               'Алабуга': '68587',
-               'Алгоритмика': '2657797',
-               'Lad': '93051',
-               'NAUMEN': '42600',
-               'Тензор': '67611',
-               'Positive Technologies': '26624',
-               'Риверстарт': '1262880',
-               'Сбер': '3529'}
+        company = {'ЭкоПоинт': '5864001',
+                   'ООО АСД Технолоджиз': '5088268',
+                   'Алабуга': '68587',
+                   'Алгоритмика': '2657797',
+                   'Lad': '93051',
+                   'NAUMEN': '42600',
+                   'Тензор': '67611',
+                   'Positive Technologies': '26624',
+                   'Риверстарт': '1262880',
+                   'Сбер': '3529'}
 
-    for i in company:
-        data = Get_employers(company[i])
-        data = data.get_datas()
-        empl_id = data['id']
-        empl_name = data['name']
-        open_vacancies = data['open_vacancies']
-        data = Get_vacancies(company[i])
-        data = data.get_datas()
-        data_e = [empl_id, empl_name, open_vacancies]
-        tbl.adding_employers(data_e)
+        for i in company:
+            data = Get_employers(company[i])
+            data = data.get_datas()
+            empl_id = data['id']
+            empl_name = data['name']
+            open_vacancies = data['open_vacancies']
+            data = Get_vacancies(company[i])
+            data = data.get_datas()
+            data_e = [empl_id, empl_name, open_vacancies]
+            tbl.adding_employers(data_e)
 
-        for i in data:
-            vacancy_id = i['id']
-            employer_id = i['employer']['id']
-            vac_name = i['name']
-            salary_from = i['salary']['from']
-            salary_to = i['salary']['to']
-            url = i['alternate_url']
-            data_v = [vacancy_id, employer_id, vac_name, salary_from, salary_to, url]
-            tbl.adding_vacancy(data_v)
+            for i in data:
+                vacancy_id = i['id']
+                employer_id = i['employer']['id']
+                vac_name = i['name']
+                salary_from = i['salary']['from']
+                salary_to = i['salary']['to']
+                url = i['alternate_url']
+                data_v = [vacancy_id, employer_id, vac_name, salary_from, salary_to, url]
+                tbl.adding_vacancy(data_v)
+    except:
+        print('База данных уже существует, можете приступить к выборке данных.')
 
     inp = input('Для получения списка компаний с указанием количества вакансий у каждой компании, введите цифру 1.\n')
     if inp == '1':
